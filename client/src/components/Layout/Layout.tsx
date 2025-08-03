1466dd8f06129eef073b6d71dbb9342bfa31c0e5
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-08-03 13:09:15 +0300

    layout

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
 import Navbar from './Navbar';
+import Breadcrumb from '../Common/Breadcrumb';
+import FloatingNavigation from '../Common/FloatingNavigation';
 
 const Layout: React.FC = () => {
   return (
@@ -9,12 +11,16 @@ const Layout: React.FC = () => {
       {/* Navigation Bar */}
       <Navbar />
       
+      {/* Breadcrumb Navigation */}
+      <Breadcrumb />
+      
       {/* Main Content */}
       <Box 
         component="main" 
         sx={{ 
           flex: 1, 
           p: { xs: 2, md: 3 },
+          pt: { xs: 1, md: 2 }, // Reduced top padding since breadcrumb provides spacing
           bgcolor: 'background.default',
           minHeight: 'calc(100vh - 64px)', // Subtract navbar height
         }}
@@ -23,6 +29,9 @@ const Layout: React.FC = () => {
           <Outlet />
         </Container>
       </Box>
+      
+      {/* Floating Navigation */}
+      <FloatingNavigation />
     </Box>
   );
 };
